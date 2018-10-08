807984c08d48ee94d9797573947628d84037b668
Elephant_Man <grogan.charlie@gmail.com>
2018-10-08 10:18:05 -0500

    server update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
