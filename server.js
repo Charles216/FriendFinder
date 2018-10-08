a1a00f3a299807f8d5976da41d4209fb8bb59f2f
Elephant_Man <grogan.charlie@gmail.com>
2018-10-08 10:20:20 -0500

    server update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "app/public")));
 
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
